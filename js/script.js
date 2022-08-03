1130c2a498a6f69ee4e1c67d60c7913a437ee7d0
Muhammad <bmuhamad915@gmail.com>
2022-08-03 04:11:11 +0300

    add new page

--- js/script.js
+++ js/script.js
@@ -280,6 +280,37 @@ document.addEventListener("DOMContentLoaded", () => {
   popup();
 
   /******************* swiper **************** */
+  let swImgBl = new Swiper(".img-bl__container", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    // slidesView: 4,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    direction: "vertical",
+
+    // mousewheel: {
+    //   sensitivity: 1,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
   let swMainContent = new Swiper(".main-top-swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
